5514316a56ebbbadc3922bcb9a9c43eb6479f9c1
Michael Kutý <6du1ro.n@gmail.com>
2015-10-17 23:12:16 +0200

    fix missing session

--- horizon/files/horizon_settings/_local_settings.py
+++ horizon/files/horizon_settings/_local_settings.py
@@ -108,7 +108,12 @@ AUTHENTICATION_BACKENDS = ('csb_auth.backend.CSBackend',)
 {%- endif %}
 
 {%- if app.murano_api is defined %}
-MURANO_API_URL = "{{ app.murano_api.get("protocol", "http") }}://{{ app.murano_api.host }}:{{ app.murano_api.port }}"
+MURANO_API_URL = "http://{{ app.murano_api.host }}:{{ app.murano_api.port }}"
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# TODO(majklk) make this configurable
+MURANO_REPO_URL = 'http://storage.apps.openstack.org/'
+# this folder muset exists before start
+#METADATA_CACHE_DIR = '/var/cache/muranodashboard-cache'
 {%- endif %}
 
 {%- if app.helpdesk_api is defined %}
