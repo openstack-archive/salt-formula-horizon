d5af4d0dd0113980efe3bbd2e89786060104f04e
Adam Tengler <a.tengler@tcpcloud.eu>
2015-11-24 19:19:27 +0100

    Enabled file includes CSS and JS files

--- horizon/files/enabled/telemetry_dashboard.py
+++ horizon/files/enabled/telemetry_dashboard.py
@@ -6,3 +6,15 @@ ENABLED = True
 ADD_INSTALLED_APPS = [
     'horizon_telemetry'
 ]
+
+ADD_SCSS_FILES = [
+    'telemetry/scss/telemetry.scss'
+]
+
+ADD_JS_FILES = [
+    'telemetry/js/cubism.js',
+    'telemetry/js/graph_utils.js',
+    'telemetry/js/concentric_gauge_area.js',
+    'telemetry/js/nv.d3.min.js'
+]
+
