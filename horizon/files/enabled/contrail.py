778950fa7c470b62a3b61ad53262ba81a29053f5
Adam Tengler <a.tengler@tcpcloud.eu>
2015-12-07 17:07:24 +0100

    Changed Contrail enabled file

---
+++ horizon/files/enabled/contrail.py
@@ -0,0 +1,9 @@
+# The name of the dashboard to be added to HORIZON['dashboards']. Required.
+DASHBOARD = 'contrail'
+# If set to True, this dashboard will be set as the default dashboard.
+DEFAULT = False
+# A dictionary of exception classes to be added to HORIZON['exceptions'].
+ADD_EXCEPTIONS = {}
+# A list of applications to be added to INSTALLED_APPS.
+ADD_INSTALLED_APPS = ['contrail_openstack_dashboard.contrail']
+
