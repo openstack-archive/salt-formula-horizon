cbf6aca67020f3a0fefb600ffa3a0e56238f1392
Adam Tengler <a.tengler@tcpcloud.eu>
2016-01-20 22:26:16 +0100

    Billing enabled file updated

--- horizon/files/enabled/billing.py
+++ horizon/files/enabled/billing.py
@@ -2,5 +2,17 @@
 DASHBOARD = 'billing'
 
 ENABLED = True
+DEFAULT = True
 
-ADD_INSTALLED_APPS = ['horizon_billing']
+ADD_INSTALLED_APPS = [
+    'horizon_billing',
+]
+
+ADD_SCSS_FILES = [
+    'billing/scss/billing.scss'
+]
+
+ADD_JS_FILES = [
+    'billing/js/nv.d3.min.js'
+]
+
